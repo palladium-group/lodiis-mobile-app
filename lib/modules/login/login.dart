aa6a3f125298d7fc6ede9bf3d4b69b6daaf06da0
Motloheloa <motloheloamaieane@gmail.com>
2025-04-25 10:58:42 +0200

    Update app_info_reference to point to production and update app version

--- lib/modules/login/login.dart
+++ lib/modules/login/login.dart
@@ -17,8 +17,8 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-  final String appLabel = "Training App";
-  final String translatedAppLabel = "App ea boikoetliso";
+  final String appLabel = "";
+  final String translatedAppLabel = "";
   late StreamSubscription connectionSubscription;
   @override
   void initState() {
