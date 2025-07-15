ba269ff8dc5d4d4edd2b785b2b1474c520bcd6ff
Motloheloa <motloheloamaieane@gmail.com>
2025-07-15 09:12:40 +0200

    Upgrade to v2.3.8

--- lib/core/constants/app_info_reference.dart
+++ lib/core/constants/app_info_reference.dart
@@ -1,9 +1,9 @@
 class AppInfoReference {
-  static const String baseUrl = 'lodiis.org';
-  static const String subBaseUrl = '';
-  static const String androidId = 'org.hisptanzania.kb_mobile_app';
-  static const String currentAppName = 'LODIIS';
-
+  static const String baseUrl = 'test.lodiis.org';
+  static const String subBaseUrl = 'test';
+  static const String androidId = 'org.hisptanzania.kb_mobile_app.testing';
+  static const String currentAppName = '[TEST] LODIIS';
+  static const String currentAppVersion = '2.3.8';
   static const String appStoreVersionPreferenceKey = 'appStoreVersion';
   static const int minimumAllowedMajorVersion = 0;
   static const int minimumAllowedMinorVersion = 0;
