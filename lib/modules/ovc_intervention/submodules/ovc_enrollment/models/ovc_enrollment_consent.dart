fb4dd3d6818e7756c3e490d472f7fabfaab1d02b
Motloheloa <motloheloamaieane@gmail.com>
2025-08-05 11:44:17 +0200

    Configure new consent form

--- lib/modules/ovc_intervention/submodules/ovc_enrollment/models/ovc_enrollment_consent.dart
+++ lib/modules/ovc_intervention/submodules/ovc_enrollment/models/ovc_enrollment_consent.dart
@@ -49,80 +49,51 @@ class OvcEnrollmentConsent {
           valueType: 'DATE',
           allowFuturePeriod: false,
         ),
-        InputField(
-            id: 'OVaqHW5kimy',
-            name:
-                'Do you understand the goal of the Bokamoso OVC Program and its potential benefits to your household?',
-            translatedName:
-                'Ke utloisisa sepheo sa morero oa bana ba tlokotsing ba Karabo ea Bophelo le melemo ea eona ho lelapa laka',
-            valueType: 'BOOLEAN',
-            labelColor: const Color(0xFF1A3518),
-            inputColor: const Color(0xFF4B9F46)),
-        InputField(
-            id: 'JCI4nxcE4N6',
-            name:
-                'Does your household agrees to regularly meet with CMW to discuss issues faced, set goals for future, and plan how to achieve those goals?',
-            translatedName:
-                'Lelapa leso le lumela ho kopana khafetsa le Case Management Worker ho buisana ka mathata ao re tobaneng le ona, ho ipehela lipheo tsa bokamoso ba rona, le hore na re ka fihlela sepheo seo joang, re sebetsa \'moho ho fihlela re fihlela sepheo sa rona?',
-            valueType: 'BOOLEAN',
-            labelColor: const Color(0xFF1A3518),
-            inputColor: const Color(0xFF4B9F46)),
-        InputField(
-            id: 'XVRQaLDDSpx',
-            name:
-                'Do you allow your information to be stored in a Case Management Information System ',
-            translatedName:
-                'Ke lumela hore litaba tsa rona li bolokoe ke morero oa Karabo ea Bophelo sebakeng se bolokehileng. Sena se bolela hore Mokhatlo o sebetsang le uena o tla boloka litaba tsa hau tsohle tsa ts’ebetso, ‘mmoho le methati eohle e tla lateloa sebakeng se bolokehileng. Litaba tsa hau li ka arolelanoa feela le mekhatlo eo o faneng ka tumello.',
-            valueType: 'BOOLEAN',
-            labelColor: const Color(0xFF1A3518),
-            inputColor: const Color(0xFF4B9F46)),
-        InputField(
-            id: 'gCdkCgKJhng',
-            name:
-                'Do you allow specific information on your household’s and child(ren) information to be shared with and discussed amongst (called case conferencing) the service provider(s) (organizations or groups).',
-            translatedName:
-                'Ke fana ka tumello ea hore litaba tse tobileng lelapa laka le bana baka li ka arolelanoa le ho buisanoa le mekhatlo e meng ho fana ka litsebeletso molemong oa ho ntlafatsa bophelo ba lelapa la ka le bana ba ka.',
-            valueType: 'BOOLEAN',
-            labelColor: const Color(0xFF1A3518),
-            inputColor: const Color(0xFF4B9F46)),
-        InputField(
-            id: 'fxqfSmoLBvT',
-            name:
-                'Have you agreed to be referred to help you  and your family achieve our goals',
-            translatedName:
-                'ke lumetse hore ke fetisetsoe lits’ebeletsong tse ling ho thusa nna le lelapa laka ho fihlela sepheo sa rona. Kea utloisisa hore litaba tse arolelanoeng litla bolokoa ele lekunutu ka tlhompho \'me li arolelanoe feela ka mokhoa o hlokahalang ho fana ka thuso eo ke e kopang kapa eo ke e hlokang        ',
-            valueType: 'BOOLEAN',
-            labelColor: const Color(0xFF1A3518),
-            inputColor: const Color(0xFF4B9F46)),
-        InputField(
-            id: 'R026OBBkvLi',
-            name:
-                'Do you understand that you can change your mind and decide not to share information',
-            translatedName:
-                'Kea utloisisa hore nka fetola maikutlo a ka mme ka etsa qeto ea ho se arolelane litaba',
-            valueType: 'BOOLEAN',
-            labelColor: const Color(0xFF1A3518),
-            inputColor: const Color(0xFF4B9F46)),
-        InputField(
-            id: 'MP7ROUSWfT9',
-            name:
-                'Do you understand that in life-threatening or emergency situations your information will be shared with necessary authorities whether or not you have given consent',
-            translatedName:
-                'Kea utloisisa hore maemong a tlokotsi kapa a tšohanyetso, litaba tsa rona litla arolelanoa le ba boholong hore na ke fane ka tumello kapa che. (Maemo a ts’osang a bophelo kapa maemo a tšohanyetso a akaretsa empa ha felle feela maemong a tlhekefetso ea bana, ho loants’ana, ho ………… kapa ho hlekefetsoa.) ',
-            valueType: 'BOOLEAN',
-            labelColor: const Color(0xFF1A3518),
-            inputColor: const Color(0xFF4B9F46)),
+
       ]),
-      FormSection(name: '', color: const Color(0xFF737373), inputFields: [
-        InputField(
+      FormSection(
+        name: 'Consent Statement',
+        color: const Color(0xFF737373),
+        description: '''
+I understand the goal of the Bokamoso OVC program as supporting healthy lives of families and its potential benefits to my household.
+
+My household agrees to regularly meet with a case management worker to:
+• Discuss issues we face
+• Set goals for our future
+• Plan how to achieve those goals
+
+This process is referred to as “case management.”
+
+I allow our information to be stored in the m2m case management system. This means m2m will keep records about our participation and progress during case management in a safe place. Only authorized individuals and those bound by shared confidentiality agreements, will access this information.
+
+I consent to the sharing and discussion of my household’s and children’s information with service providers (organizations/groups) for case conferencing. This will only occur on a need-to-know basis and with respect for confidentiality to help us achieve our goals.
+
+I understand:
+• I may withdraw this consent at any time.
+• In life-threatening or emergency situations, our information may be shared with authorities even without my consent (e.g., health service denial or refusal to adhere to essential health services).
+''',
+        translatedDescription: '''
+Mosebeletsi oa morero o oa bala: “Thepa ke lintho tse molemo li bile li le bohlokoa ho oena. 
+Mohlala e ka ba batho hobane motho ka mong o na le litsebo, mahlale le litalenta tse itseng.”
+''',
+        inputFields: [],
+      ),
+
+      FormSection(
+        name: 'Consent Confirmation',
+        color: const Color(0xFF737373),
+        inputFields: [
+          InputField(
             id: 'sCGr0RTmvJ7',
-            name: 'Are you willing to be among beneficiaries of this program?',
-            translatedName:
-                'Na u ikemiselitse ho ba e mong oa bajalefa ba lenaneo lee?',
+            name: 'I am willing to participate in this program, and I understand that my participation is voluntary. I may withdraw my consent at any time without affecting my access to other community services.',
+            translatedName: 'Na u ikemiselitse ho ba e mong oa bajalefa ba lenaneo lee?',
             valueType: 'BOOLEAN',
-            labelColor: const Color(0xFF1A3518),
-            inputColor: const Color(0xFF4B9F46))
-      ])
+            labelColor: const Color(0xFF737373),
+            inputColor: const Color(0xFF737373),
+          ),
+        ],
+      )
+
     ];
   }
 }
