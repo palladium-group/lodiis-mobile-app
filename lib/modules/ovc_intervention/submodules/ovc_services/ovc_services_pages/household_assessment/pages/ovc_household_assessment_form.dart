0ae39d113a131f7d4ae13eedeec5de84bc831d3f
Lefa Mafoso <mafosolefa@gmail.com>
2025-08-12 16:18:54 +0200

    Add new data elements for HH assessment

--- lib/modules/ovc_intervention/submodules/ovc_services/ovc_services_pages/household_assessment/pages/ovc_household_assessment_form.dart
+++ lib/modules/ovc_intervention/submodules/ovc_services/ovc_services_pages/household_assessment/pages/ovc_household_assessment_form.dart
@@ -109,7 +109,7 @@ class _OvcHouseholdAssessmentFormState
             Provider.of<ServiceFormState>(context, listen: false).formState;
 
         await OvchouseHoldAssessmentSkipLogic.evaluateSkipLogics(
-            context, formSections!, dataObject, adult?.hivStatus);
+            context, formSections!, dataObject, adult?.hivStatus, adult?.artStatus);
       },
     );
   }
