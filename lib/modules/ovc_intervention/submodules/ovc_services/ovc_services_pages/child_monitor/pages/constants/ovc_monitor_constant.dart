1f079b6530d36acaba786b6b9cb59009915d4989
Motloheloa <motloheloamaieane@gmail.com>
2025-09-21 21:25:25 +0200

    Remove unused files

--- lib/modules/ovc_intervention/submodules/ovc_services/ovc_services_pages/child_monitor/pages/constants/ovc_monitor_constant.dart
+++
@@ -1,8 +0,0 @@
-class OvcMonitorConstant {
-  static Map getOvcMonitorProgramStageMap() {
-    Map map = {};
-    map['ZHjrdThi7Bz'] = 'Service';
-    map['dVdlJNtNLIQ'] = 'School';
-    return map;
-  }
-}
